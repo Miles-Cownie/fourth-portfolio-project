659ac717d90881a1316f9970b6e482a4265ca224
Miles-Cownie <miles_cownie94@protonmail.com>
2022-11-16 15:18:19 +0000

    Add Skills and Comments Model

--- fnvbuildapp/models.py
+++ fnvbuildapp/models.py
@@ -116,6 +116,119 @@ class Perks(models.Model):
     def __str__(self):
         return self.perk_name
 
+# Starting Skills Model
+
+
+class StartingSkills(models.Model):
+    barter = models.IntegerField(
+        default=15,
+        validators=[
+            MinValueValidator(5),
+            MaxValueValidator(52)
+            ]
+        )
+    energy_weapons = models.IntegerField(
+        default=15,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(47)
+            ]
+        )
+    explosives = models.IntegerField(
+        default=15,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(47)
+            ]
+        )
+    guns = models.IntegerField(
+        default=15,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(47)
+            ]
+        )
+    lockipick = models.IntegerField(
+        default=15,
+        validators=[
+            MinValueValidator(5),
+            MaxValueValidator(47)
+            ]
+        )
+    medicine = models.IntegerField(
+        default=15,
+        validators=[
+            MinValueValidator(5),
+            MaxValueValidator(52)
+            ]
+        )
+    melee_weapons = models.IntegerField(
+        default=15,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(47)
+            ]
+        )
+    repair = models.IntegerField(
+        default=15,
+        validators=[
+            MinValueValidator(5),
+            MaxValueValidator(52)
+            ]
+        )
+    science = models.IntegerField(
+        default=15,
+        validators=[
+            MinValueValidator(5),
+            MaxValueValidator(52)
+            ]
+        )
+    sneak = models.IntegerField(
+        default=15,
+        validators=[
+            MinValueValidator(5),
+            MaxValueValidator(47)
+            ]
+        )
+    speech = models.IntegerField(
+        default=15,
+        validators=[
+            MinValueValidator(5),
+            MaxValueValidator(52)
+            ]
+        )
+    survival = models.IntegerField(
+        default=15,
+        validators=[
+            MinValueValidator(5),
+            MaxValueValidator(47)
+            ]
+        )
+    unarmed = models.IntegerField(
+        default=15,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(47)
+            ]
+        )
+
+    def __str__(self):
+        return [
+            self.barter,
+            self.energy_weapons,
+            self.explosives,
+            self.guns,
+            self.lockipick,
+            self.medicine,
+            self.melee_weapons,
+            self.repair,
+            self.science,
+            self.sneak,
+            self.speech,
+            self.survival,
+            self.unarmed
+            ]
+
 # Character Build Model
 
 
@@ -133,10 +246,10 @@ class CharacterBuild(models.Model):
     description = models.TextField()
     build_gender = models.ManyToManyField(Gender)
     build_special = models.ManyToManyField(Special)
-    build_traits = models.ManyToManyField(Traits)
+    build_traits = models.ManyToManyField(Traits, blank=True)
     build_tag_skills = models.ManyToManyField(TagSkills)
-    build_perks = models.ManyToManyField(Perks)
-    build_starting_skills = models.ManyToManyField(StartingSkills)
+    build_perks = models.ManyToManyField(Perks, blank=True)
+    build_starting_skills = models.ManyToManyField(StartingSkills, blank=True)
     likes = models.ManyToManyField(
         User, related_name='fnv_build_like', blank=True
         )
@@ -149,3 +262,22 @@ class CharacterBuild(models.Model):
 
     def total_likes(self):
         return self.likes.count()
+
+# Character Build Comment Model
+
+
+class Comment(models.Model):
+    charactercomment = models.ForeignKey(
+        CharacterBuild, on_delete=models.CASCADE, related_name="comments"
+        )
+    name = models.CharField(max_length=90)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}."
