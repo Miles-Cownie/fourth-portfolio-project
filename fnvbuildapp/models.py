40595f436793d74b1674184e3b2b7d25fef99dd2
Miles-Cownie <miles_cownie94@protonmail.com>
2022-11-16 13:55:28 +0000

    Add Gender and SPECIAL models

--- fnvbuildapp/models.py
+++ fnvbuildapp/models.py
@@ -1,10 +1,87 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from cloudinary.models import CloudinaryField
 
 # Status variable
 
 STATUS = ((0, "Draft"), (1, "Awaiting Approval"), (2, "Published"))
+
+
+class Gender(models.Model):
+    GENDER_CHOICES = (
+        ("M", "Male"),
+        ("F", "Female"),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return self.gender
+
+# SPECIAL stats Model
+
+
+class Special(models.Model):
+    strength = models.IntegerField(
+        default=5,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+            ]
+        )
+    perception = models.IntegerField(
+        default=5,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+            ]
+        )
+    endurance = models.IntegerField(
+        default=5,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+            ]
+        )
+    charisma = models.IntegerField(
+        default=5,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+            ]
+        )
+    intelligence = models.IntegerField(
+        default=5,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+            ]
+        )
+    agility = models.IntegerField(
+        default=5,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+            ]
+        )
+    luck = models.IntegerField(
+        default=5,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+            ]
+        )
+
+    def __str__(self):
+        return [
+            self.strength,
+            self.perception,
+            self.endurance,
+            self.charisma,
+            self.agility,
+            self.intelligence,
+            self.luck
+            ]
 
 # Character Build Model
 
@@ -21,12 +98,12 @@ class CharacterBuild(models.Model):
     featured_image = CloudinaryField('Build Image', default='placeholder')
     excerpt = models.TextField(blank=True)
     description = models.TextField()
-    build_gender = models.ManyToManyField('Gender')
-    build_special = models.ManyToManyField('Special')
-    build_traits = models.ManyToManyField('Traits')
-    build_tag_skills = models.ManyToManyField('TagSkills')
-    build_perks = models.ManyToManyField('Perks')
-    build_starting_skills = models.ManyToManyField('StartingSkills')
+    build_gender = models.ManyToManyField(Gender)
+    build_special = models.ManyToManyField(Special)
+    build_traits = models.ManyToManyField(Traits)
+    build_tag_skills = models.ManyToManyField(TagSkills)
+    build_perks = models.ManyToManyField(Perks)
+    build_starting_skills = models.ManyToManyField(StartingSkills)
     likes = models.ManyToManyField(
         User, related_name='fnv_build_like', blank=True
         )
