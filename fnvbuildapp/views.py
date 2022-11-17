2e0e94fa87809cdb05666d031c5b5ede7940c6b6
Miles-Cownie <miles_cownie94@protonmail.com>
2022-11-17 13:21:56 +0000

    Add initial view

--- fnvbuildapp/views.py
+++ fnvbuildapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import CharacterBuild
 
-# Create your views here.
+# List of Character Builds
+
+
+class BuildList(generic.ListView):
+    model = CharacterBuild
+    queryset = CharacterBuild.objects.filter(status=2).order_by('-created_on')
+    template_name = "index.html"
+    paginate_by = 8
