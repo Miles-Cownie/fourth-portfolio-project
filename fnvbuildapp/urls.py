46a2fc53a232aa2f85eb6dac7139f49b95553d4e
Miles-Cownie <miles_cownie94@protonmail.com>
2022-11-21 16:04:06 +0000

    Fix template render bug

--- fnvbuildapp/urls.py
+++ fnvbuildapp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", views.BuildList.as_view(), name='home')
+    path('', views.BuildList.as_view(), name='home')
 ]
--- fnvbuildsite/urls.py
+++ fnvbuildsite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('fnvbuildapp.urls'), name='fnvbuildapp-urls'),
+    path('', include('fnvbuildapp.urls'), name='fnvbuildapp-urls'),
     path('summernote/', include('django_summernote.urls')),
 ]
