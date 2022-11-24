eaaad6cb44b72089d75afdc5b1b418e3de26f57c
Miles-Cownie <miles_cownie94@protonmail.com>
2022-11-24 14:29:23 +0000

    Wire up build detail page

--- fnvbuildapp/urls.py
+++ fnvbuildapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.BuildList.as_view(), name='home')
+    path('', views.BuildList.as_view(), name='home'),
+    path('slug:slug/', views.BuildDetail.as_view(), name='post_detail'),
 ]
