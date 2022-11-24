1673d4a0e034061e3f97f762c44a8e2b46e806f0
Miles-Cownie <miles_cownie94@protonmail.com>
2022-11-24 11:23:25 +0000

    Add build detail view

--- fnvbuildapp/views.py
+++ fnvbuildapp/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import CharacterBuild
 
-# List of Character Builds
+# Home Page View
 
 
 class BuildList(generic.ListView):
@@ -10,3 +10,26 @@ class BuildList(generic.ListView):
     queryset = CharacterBuild.objects.filter(status=2).order_by('-created_on')
     template_name = "index.html"
     paginate_by = 6
+
+# Detailed Page View
+
+
+class BuildDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=2)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "build_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            }
+        )
