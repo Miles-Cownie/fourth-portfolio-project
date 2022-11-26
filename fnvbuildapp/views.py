adadf5fff2890c3cf3659bfbb7e444a6de83dd0f
Miles-Cownie <miles_cownie94@protonmail.com>
2022-11-26 19:40:44 +0000

    add comment functionality

--- fnvbuildapp/views.py
+++ fnvbuildapp/views.py
@@ -31,7 +31,39 @@ class BuildDetail(View):
             {
                 "build": build,
                 "comments": comments,
+                "commented": False,
                 "liked": liked,
                 "comment_form": CommentForm()
-            }
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = CharacterBuild.objects.filter(status=2)
+        build = get_object_or_404(queryset, slug=slug)
+        comments = build.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if build.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.build = build
+            commnet.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "build_detail.html",
+            {
+                "build": build,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
         )
